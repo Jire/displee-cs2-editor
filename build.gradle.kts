400a9157488e791eb6a7ef53a905fd4b42d58053
Jire <thomasgnappo@gmail.com>
2023-01-30 22:07:03 -0500

    chore: upgraded JVM version to 19

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(19))
         vendor.set(JvmVendorSpec.BELLSOFT)
     }
 }
@@ -18,15 +18,15 @@ dependencies {
     implementation("com.displee:disio:2.2")
     implementation("com.displee:rs-cache-library:6.8.1")
 
-    val jettyVersion = "11.0.12"
+    val jettyVersion = "11.0.13"
     implementation("org.eclipse.jetty:jetty-io:$jettyVersion")
     implementation("org.eclipse.jetty:jetty-server:$jettyVersion")
     implementation("org.eclipse.jetty:jetty-http:$jettyVersion")
     implementation("org.eclipse.jetty:jetty-util:$jettyVersion")
 
     implementation("javax.servlet:javax.servlet-api:4.0.1")
-    implementation("org.fxmisc.richtext:richtextfx:0.10.9")
-    implementation("com.google.code.gson:gson:2.10")
+    implementation("org.fxmisc.richtext:richtextfx:0.11.0")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.apache.commons:commons-lang3:3.12.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
